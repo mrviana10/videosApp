4766fb483230de6448954feef15e88096e684ca8
Marcos Roberto Viana <marcaougo2008@hotmail.com>
2021-06-05 22:17:43 -0300

    Adicionamos array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.models';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -9,7 +10,44 @@ import { ToastController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController, public toastController: ToastController) {}
+  titulo = 'Videos';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021',
+      duracao: '1h 50m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ijvC2w2yANsfgLT3LMu2zFr0fxh.jpg',
+      generos:['Ação', 'Fantasia', 'Aventura']
+    },
+    {
+      nome: 'Liga da Justiça de Zack Snyder (2021)',
+      lancamento: '15/04/2021',
+      duracao: '4h 2m',
+      classificacao: 85,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ArWn6gCi61b3b3hclD2L0LOk66k.jpg',
+      generos: ['Ação', 'Aventura', 'Fantasia', 'Ficção científica']
+    },
+    {
+      nome: 'Raya e o Último Dragão (2021)',
+      lancamento: '04/03/2021',
+      duracao: '1h 47m',
+      classificacao: 82,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/o2NTWpD6LVf1YyPKTdvcEuHqcJ6.jpg',
+      generos: ['Animação', 'Aventura', 'Fantasia', 'Família', 'Ação']
+    },
+    {
+      nome: 'Contra a Terra (2021)',
+      lancamento: '30/04/2021',
+      duracao: '5m',
+      classificacao: 71,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yXhjueyyNNrGinwM9WIxuwNqzbm.jpg',
+      generos: ['Família', 'Animação', 'Comédia']
+    }
+  ];
+
+  constructor(public alertController: AlertController, public toastController: ToastController) { }
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
